1daba985f5c8d440aca5f3779fac16e7a49498e2
Don Cameron <donrcameron@gmail.com>
2019-08-12 19:27:48 +1000

    add_templates
    Add templates, upgrade appearence

--- develop/application.py
+++ develop/application.py
@@ -3,36 +3,26 @@ Flask model to display file contents
 '''
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello() -> str:
-    return "Bonjour Tout les Monde de Azure!"
+@app.route("/index")
+def index() -> str:
+    return render_template('index.html', title='My Working Title', joke='Bonjour, tout les monde')
 
-
-# end hello
+# end index
 
 
 @app.route("/file")
 def show_file() -> str:
-    wrap1 = (
-        "<!DOCTYPE html>"
-        + "<html>"
-        + "<body> "
-        + "<p> The file contents will be shown below </p>"
-        + " <br/>"
-    )
-    text = ""
+
     with open("showable.txt") as file:
-        for line in file:
-            text = text + line + "<br/>"
-        # end for
+        lines = [line for line in file]
+    #end with
 
-    tail1 = "</body>" + "</html>"
-
-    return wrap1 + text + tail1
-
+    return render_template('file_contents.html', lines=lines)
 
 # end show_file
